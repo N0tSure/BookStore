d729bf81f45e7fda222ad91748a64a364b08b803
NotSure <ASir2089@gmail.com>
2016-11-24 01:35:37 +0300

    Injected BookService into BootController

--- src/main/java/org/books/controller/BookController.java
+++ src/main/java/org/books/controller/BookController.java
@@ -1,5 +1,7 @@
 package org.books.controller;
 
+import org.books.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,23 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+    private BookService service;
+
+    @Autowired
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
     @RequestMapping(value = "/book/all", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public String getAllBooks() {
-        return "All success";
+        return service.getAllBooks();
     }
 
     @RequestMapping(value = "/book/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public String addBook(@RequestBody String json) {
-        return "Add success: " + json;
+        return service.addBook(json);
     }
 
     @RequestMapping(value = "/book/remove", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public String removeBook(@RequestBody String json) {
-        return "Remove success: " + json;
+    public String removeBook(@RequestBody String request) {
+        return service.removeBook(request);
     }
 
     @RequestMapping(value = "/book/update", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public String updateBook(@RequestBody String json) {
-        return "Update success: " + json;
+        return service.updateBook(json);
     }
 }
