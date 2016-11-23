518a38e6f303ff8a86af3dea930c0a5af10ca592
NotSure <ASir2089@gmail.com>
2016-11-23 20:52:04 +0300

    Hibernate was set up
    Added methods for bean, and seting of properties was proceed. Also added
    property file

--- src/main/java/org/books/config/HibernateConfig.java
+++ src/main/java/org/books/config/HibernateConfig.java
@@ -1,7 +1,71 @@
 package org.books.config;
 
-/**
- * Created by cresh on 23.11.16.
- */
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"org.books.config"})
+@PropertySource(value = {"classpath:hibernate.properties"})
 public class HibernateConfig {
+
+    private Environment environment;
+
+    @Autowired
+    public HibernateConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    @Autowired
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource);
+        localSessionFactoryBean.setPackagesToScan("org.books.model");
+        localSessionFactoryBean.setHibernateProperties(getHibernateProperties());
+        return localSessionFactoryBean;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
+        managerDataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        managerDataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        managerDataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        managerDataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return managerDataSource;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor postProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
 }
