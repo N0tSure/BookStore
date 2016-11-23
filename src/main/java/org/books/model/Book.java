472b6aa8f692bc1e795081ea9367a070790c5de3
NotSure <ASir2089@gmail.com>
2016-11-23 20:53:14 +0300

    Entity for book was added

--- src/main/java/org/books/model/Book.java
+++ src/main/java/org/books/model/Book.java
@@ -1,7 +1,109 @@
 package org.books.model;
 
-/**
- * Created by cresh on 23.11.16.
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private Float price;
+    private String description;
+    private String isbn;
+    private Integer pages;
+
+    public Book() {
+    }
+
+    public Book(String title, Float price, String description, String isbn, Integer pages) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.isbn = isbn;
+        this.pages = pages;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (id != null ? !id.equals(book.id) : book.id != null) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        if (price != null ? !price.equals(book.price) : book.price != null) return false;
+        if (description != null ? !description.equals(book.description) : book.description != null) return false;
+        if (isbn != null ? !isbn.equals(book.isbn) : book.isbn != null) return false;
+        return pages != null ? pages.equals(book.pages) : book.pages == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (isbn != null ? isbn.hashCode() : 0);
+        result = 31 * result + (pages != null ? pages.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("#%d: %s, %d pages, %.2f roubles", id, title, pages, price);
+    }
 }
