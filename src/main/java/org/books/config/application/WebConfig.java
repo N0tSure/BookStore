93f6118642ab6590dc87b5e5df91eb55f908c852
NotSure <ASir2089@gmail.com>
2016-11-25 14:35:27 +0300

    Added component scan to WebConfig to resolving beans in packages org.books.controller, org.books.config, org.books.service

--- src/main/java/org/books/config/application/WebConfig.java
+++ src/main/java/org/books/config/application/WebConfig.java
@@ -34,6 +34,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/views/css/fonts/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/views/fonts/");
     }
 }
